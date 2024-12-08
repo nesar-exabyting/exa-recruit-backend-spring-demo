b90daac22c199c906ce8f38ceec950c2b24eb5ec
nesar-exabyting <nesar@exabyting.com>
2024-12-08 18:40:21 +0600

    project setup

---
+++ src/main/java/com/exabyting/exa_recruit/ExaRecruitApplication.java
@@ -0,0 +1,13 @@
+package com.exabyting.exa_recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaRecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaRecruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exabyting/exa_recruit/ExaRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exabyting.exa_recruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaRecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
